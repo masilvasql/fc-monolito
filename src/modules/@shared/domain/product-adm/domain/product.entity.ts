2429e27e7fbd6e39b7898328a01ef0ba996e6aa0
marcelo.silva <marcelo.silva@neomind.com.br>
2023-04-25 21:59:36 -0300

    iniciando o projeto

---
+++ src/modules/@shared/domain/product-adm/domain/product.entity.ts
@@ -0,0 +1,62 @@
+import AggregateRoot from "../../entities/aggregate-root.interface";
+import BaseEntity from "../../entities/base.entity";
+import Id from "../../value-object/id.value-object";
+
+
+type ProductProps = {
+    id?: Id;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export default class Product extends BaseEntity implements AggregateRoot {
+    
+    private _name: string;
+    private _description: string;
+    private _purchasePrice: number;
+    private _stock: number;
+
+    constructor(props: ProductProps) {
+        super(props.id);
+        this._name = props.name;
+        this._description = props.description;
+        this._purchasePrice = props.purchasePrice;
+        this._stock = props.stock;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    set description(value: string) {
+        this._description = value;
+    }
+
+    get purchasePrice(): number {
+        return this._purchasePrice;
+    }
+
+    set purchasePrice(value: number) {
+        this._purchasePrice = value;
+    }
+    
+    get stock(): number {
+        return this._stock;
+    }
+
+    set stock(value: number) {
+        this._stock = value;
+    }
+}
